41db8360a6fd1a5329d57c5893335126ea3ed2a9
carles <carles.pons@akamon.com>
2014-02-25 08:09:03 +0100

    Test refactoring

--- test/GildedRoseTest.java
+++ test/GildedRoseTest.java
@@ -3,77 +3,131 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GildedRoseTest {
-    @Test
+    private static final int UPPER_LIMIT = 50;
+    private static final int NORMAL_DEGRADING_FACTOR = 1;
+    private static final int EXPIRED_DEGRADING_FACTOR = 2;
+    private static final int AGED_BRIE_AGING_FACTOR = 1;
+	private static final String ANY_ITEM = "Any Item";
+	private static final String AGED_BRIE = "Aged Brie";
+	private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+	private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+	
+	@Test
     public void eachDayQualityAndSellInDecreases() {
-        Item[] items = new Item[] { new Item("foo", 8, 6) };
+		Integer anySellIn=8;
+		Integer anyQuality=6;
+		Integer expectedSellIn=anySellIn-1;
+		Integer expectedQuality=anyQuality-NORMAL_DEGRADING_FACTOR;
+
+        Item[] items = new Item[] { new Item(ANY_ITEM, anySellIn, anyQuality) };
+        
         GildedRose app = new GildedRose(items);
         app.updateQuality();
+        
         assertEquals(7, app.items[0].sellIn);
-        assertEquals(5, app.items[0].quality);
+        assertEquals(expectedQuality, app.items[0].quality);
     }
     @Test
     public void expiredItemsDecreaseTwiceFast() {
-        Item[] items = new Item[] { new Item("foo", 0, 6) };
+		Integer sellInExpiredDate=0;
+		Integer anyQuality=6;
+		Integer expectedQuality=anyQuality-EXPIRED_DEGRADING_FACTOR;
+		
+        Item[] items = new Item[] { new Item(ANY_ITEM, sellInExpiredDate, anyQuality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(-1, app.items[0].sellIn);
-        assertEquals(4, app.items[0].quality);
+
+        assertEquals(expectedQuality, app.items[0].quality);
     }
     @Test
     public void qualityIsNeverNegative() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+		Integer zeroQuality=0;
+		Integer sellInExpiredDate=0;
+		
+        Item[] items = new Item[] { new Item(ANY_ITEM, sellInExpiredDate, zeroQuality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(0, app.items[0].quality);
+        
+        assertEquals(zeroQuality, app.items[0].quality);
     }
     @Test
     public void agedBrieIncreaseInQuality() {
-        Item[] items = new Item[] { new Item("Aged Brie", 5, 5) };
+		Integer anySellIn=8;
+		Integer anyQuality=5;
+		Integer expectedQuality=anyQuality+AGED_BRIE_AGING_FACTOR;
+		
+        Item[] items = new Item[] { new Item(AGED_BRIE, anySellIn, anyQuality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(6, app.items[0].quality);
+        
+        assertEquals(expectedQuality, app.items[0].quality);
     }
     @Test
     public void qualityHasAnUpperLimit() {
-        Item[] items = new Item[] { new Item("Aged Brie", 5, 50) };
+		Integer anySellIn=5;
+		
+        Item[] items = new Item[] { new Item(AGED_BRIE, anySellIn, UPPER_LIMIT) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(50, app.items[0].quality);
+        
+        assertEquals(UPPER_LIMIT, app.items[0].quality);
     }
     @Test
     public void sulfurasIsLegendary() {
-        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 5, 20) };
+		Integer anySellIn=8;
+		Integer anyQuality=6;
+		
+        Item[] items = new Item[] { new Item(SULFURAS_HAND_OF_RAGNAROS, anySellIn, anyQuality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(5, app.items[0].sellIn);
-        assertEquals(20, app.items[0].quality);
+        
+        assertEquals(anySellIn, app.items[0].sellIn);
+        assertEquals(anyQuality, app.items[0].quality);
     }
     @Test
     public void backstageIncreasesInQuality() {
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 20, 20) };
+		Integer longSellIn=20;
+		Integer anyQuality=6;
+		Integer expectedQuality=anyQuality+1;
+		
+        Item[] items = new Item[] { new Item(BACKSTAGE_PASSES, longSellIn, anyQuality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(21, app.items[0].quality);
+        
+        assertEquals(expectedQuality, app.items[0].quality);
     }
     @Test
     public void backstageIncreasesFasterInQualityWhenConcertAproaches() {
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 9, 20) };
+		Integer closeSellIn=9;
+		Integer anyQuality=6;
+		Integer expectedQuality=anyQuality+2;
+		
+        Item[] items = new Item[] { new Item(BACKSTAGE_PASSES, closeSellIn, anyQuality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(22, app.items[0].quality);
+        
+        assertEquals(expectedQuality, app.items[0].quality);
     }
     @Test
     public void backstageIncreasesFasterInQualityWhenConcertIsInminent() {
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 4, 20) };
+		Integer inmminetSellIn=4;
+		Integer anyQuality=6;
+		Integer expectedQuality=anyQuality+3;
+		
+        Item[] items = new Item[] { new Item(BACKSTAGE_PASSES, inmminetSellIn, anyQuality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(23, app.items[0].quality);
+        
+        assertEquals(expectedQuality, app.items[0].quality);
     }
     @Test
     public void backstageLossAllQualityWhenConcertIsOver() {
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20) };
+		Integer anyQuality=6;
+		
+        Item[] items = new Item[] { new Item(BACKSTAGE_PASSES, 0, anyQuality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
+        
         assertEquals(0, app.items[0].quality);
     }
 
